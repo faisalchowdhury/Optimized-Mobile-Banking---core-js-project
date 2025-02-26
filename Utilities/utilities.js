5b2465a3b52b019169eae4ed0a98cf939abaf78f
Faisal Chowdhury <chowdhuryfaisal66@gmail.com>
2025-02-26 15:38:56 +0600

    implemented add money feature

--- Utilities/utilities.js
+++ Utilities/utilities.js
@@ -8,6 +8,31 @@ function getValueById(id) {
     return value;
 }
 
+// making value float number 
+
+function makeInputValueFloat(id){
+
+    let value = document.getElementById(id).value;
+    value = parseFloat(value);
+
+    return value;
+}
+// making text value float number 
+
+function makeTextValueFloat(id){
+
+    let value = document.getElementById(id).innerText;
+    value = parseFloat(value);
+
+    return value;
+}
+
+// set main balance
+function setBalance(balance){
+
+    document.getElementById('main-balance').innerText = balance;
+}
+
 
 
 // toggling action boxes 
--- js/dashboard.js
+++ js/dashboard.js
@@ -4,13 +4,24 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault();
     const accountNumber = document.getElementById('add-money-account-number').value;
     const pinNumber = getValueById('add-money-pin-number');
+    const amount = makeInputValueFloat('add-money-amount');
+    const mainBalance =makeTextValueFloat('main-balance');
 
     if(accountNumber.length === 12){
  
         if(pinNumber === 1234){
            
-          
+         if(amount > 0 && !isNaN(amount)){
+             
 
+            newBalance = mainBalance + amount;
+        
+            setBalance(newBalance)
+
+         }else {
+           alert('invalid amount')
+         }
+          
         }else{
             alert('invalid pin number')
         }
